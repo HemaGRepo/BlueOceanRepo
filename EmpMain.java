2f1699d6883281b31abf0174582a5768bd5d4620
HemaGRepo <hema.g@capgemini.com>
2018-11-27 15:32:12 +0530

    V1.0

---
+++ EmpMain.java
@@ -0,0 +1,14 @@
+
+public class EmpMain {
+
+	public static void main(String[] args) {
+	 int i=0;
+	 for(i=0;i<=10;i++)
+	  System.out.println("The Blue Ocean executes......"+i);
+	  System.out.println("_________________");
+		System.out.println("Hello.");
+		System.out.println("Welcome to Java");
+		System.out.println("_________________");
+	}
+
+}
